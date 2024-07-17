931c0637d770fe1effe7de4616ce450b2d2741c0
AzizArres <63376723+AzizArres@users.noreply.github.com>
2024-07-17 18:58:19 +0100

    actual translation from an API with copy to clipboard functionality

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react"
 import speaker from "/src/assets/sound_max_fill.svg"
@@ -5,39 +6,54 @@ import copy from "/src/assets/content_copy_24dp_5F6368_FILL0_wght400_GRAD0_opsz2
 
 export default function Box(){
 
-    const [translatedLanguage, setTranslatedLanguage] = React.useState("")
+    const [translatedLanguage, setTranslatedLanguage] = React.useState("en")
 
-    const [translatorLanguage, setTranslatorLanguage] = React.useState("")
+    const [translatorLanguage, setTranslatorLanguage] = React.useState("fr")
 
-    const [charCount, setCharCount] = React.useState(0)
+    const [phrase, setPhrase] = React.useState("Hello, how are you?")
+
+    const [translatedPhrase, setTranslatedPhrase] = React.useState("")
+
+    const [data, setData] = React.useState("")
 
     function handleChange(event){
-        setCharCount (event.target.value.length)
+        setPhrase(event.target.value)
     }
+   
+    React.useEffect(() => {
+        fetch(`https://api.mymemory.translated.net/get?q=${phrase}&langpair=${translatedLanguage}|${translatorLanguage}`)
+        .then(res => res.json())
+        .then(data => {
+            setData(data)
+        })
+        .catch(error => console.log(error))
+    }, [phrase, translatedLanguage, translatorLanguage])
 
+    function translate(){
+        setTranslatedPhrase(data.responseData.translatedText)
+    }
+    
     return(
         <div className="box">
             <div className="translated">
                         <div className="languages">
                             <h3>Detect Language</h3>
-                            <button onClick={() => setTranslatedLanguage("english")} className={translatedLanguage === "english" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "english" ? true : false}>English</button>
-                            <button onClick={() => setTranslatedLanguage("french")} className={translatedLanguage === "french" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "french" ? true : false}>French</button>
-                            <button onClick={() => setTranslatedLanguage("spanish")} className={translatedLanguage === "spanish" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "spanish" ? true : false}>Spanish</button>
+                            <button onClick={() => setTranslatedLanguage("en")} className={translatedLanguage === "en" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "en" ? true : false}>English</button>
+                            <button onClick={() => setTranslatedLanguage("fr")} className={translatedLanguage === "fr" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "fr" ? true : false}>French</button>
+                            <button onClick={() => setTranslatedLanguage("es")} className={translatedLanguage === "es" ? "selected-language" : "language-button"}  disabled={translatorLanguage === "es" ? true : false}>Spanish</button>
                         </div>
-                        <textarea name="textArea" id="" className="text-area" maxLength="500" onChange={handleChange}>
-                        
-                        </textarea>
+                        <textarea name="textArea" id="" className="text-area" maxLength="500" onChange={handleChange} defaultValue={phrase} />
                         <div className="audio-and-copy">
                             <div className="audio-and-copy-buttons">
                             <button>
                                 <img src={speaker} alt="" />
                             </button>
                             <button>
-                                <img src={copy} alt="" />
+                                <img src={copy} alt="" onClick={() => navigator.clipboard.writeText(phrase)} />
                             </button>
                             </div>
-                            <h5 className="caracters-count">{charCount}/500</h5>
-                            <button className="translate-button">
+                            <h5 className="caracters-count">{phrase.length}/500</h5>
+                            <button className="translate-button" onClick={() => translate()}>
                                 Translate
                             </button>
                         </div>
@@ -45,19 +61,17 @@ export default function Box(){
                 <div className="translator">
                     <div className="languages">
                         <h3>Detect Language</h3>
-                        <button onClick={() => setTranslatorLanguage("english")} className={translatorLanguage === "english" ? "selected-language" : "language-button"} disabled={translatedLanguage === "english" ? true : false}>English</button>
-                        <button onClick={() => setTranslatorLanguage("french")} className={translatorLanguage === "french" ? "selected-language" : "language-button"} disabled={translatedLanguage === "french" ? true : false}>French</button>
-                        <button onClick={() => setTranslatorLanguage("spanish")} className={translatorLanguage === "spanish" ? "selected-language" : "language-button"} disabled={translatedLanguage === "spanish" ? true : false}>Spanish</button>
+                        <button onClick={() => setTranslatorLanguage("en")} className={translatorLanguage === "en" ? "selected-language" : "language-button"} disabled={translatedLanguage === "en" ? true : false}>English</button>
+                        <button onClick={() => setTranslatorLanguage("fr")} className={translatorLanguage === "fr" ? "selected-language" : "language-button"} disabled={translatedLanguage === "fr" ? true : false}>French</button>
+                        <button onClick={() => setTranslatorLanguage("es")} className={translatorLanguage === "es" ? "selected-language" : "language-button"} disabled={translatedLanguage === "es" ? true : false}>Spanish</button>
                     </div>
-                    <textarea name="" id="" className="text-area" maxLength="500">
-                        
-                    </textarea>
+                    <textarea name="any-name" id="" className="text-area" maxLength="500" disabled={true} value={translatedPhrase} />
                     <div className="audio-and-copy">
                         <button>
                             <img src={speaker} alt="" />
                         </button>
                         <button>
-                            <img src={copy} alt="" />
+                            <img src={copy} alt="" onClick={() => navigator.clipboard.writeText(translatedPhrase)} />
                         </button>
                     </div>
                 </div>
